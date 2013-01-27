bc4d612253359028e582eabfd9d2bdb4075017b7
James Nguyen <ja.nguyen@gmail.com>
2013-01-26 18:28:17 -0800

    Added CardMatchingGame.h/m

---
+++ Matchismo/Matchismo/Model/CardMatchingGame.h
@@ -0,0 +1,22 @@
+//
+//  CardMatchingGame.h
+//  Matchismo
+//
+//  Created by James Nguyen on 1/26/13.
+//  Copyright (c) 2013 James Nguyen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Deck.h"
+
+@interface CardMatchingGame : NSObject
+
+- (id)initWithCardCount:(NSUInteger)cardCount
+              usingDeck:(Deck *)deck;
+
+- (void)flipCardAtIndex:(NSUInteger)index;
+
+- (Card *)cardAtIndex:(NSUInteger)index;
+
+@property (nonatomic, readonly) int score;
+@end
